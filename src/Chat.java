f935f92e0e9cd1ea481cc35cba6a1a25facd0a03
ahmadatra <aatra4@uic.edu>
2017-11-29 18:50:39 -0600

    fixed merge

--- src/Chat.java
+++ src/Chat.java
@@ -1,8 +1,11 @@
+import javax.swing.JFrame;
 
 public class Chat {
 
 	public static void main(String[] args) {
 		
+		GUI gui = new GUI();
+		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 	}
 
