a8bbf2948042e5196ed2dbbb611393cda732b725
ahmadatra <aatra4@uic.edu>
2017-12-06 20:28:25 -0600

    Client/Client GUI
    Updated the code and put some code in functions

--- src/Client.java
+++ src/Client.java
@@ -9,23 +9,39 @@ public class Client {
 	PrintWriter output = null;
 	BufferedReader input = null;
 	boolean isServer = false;
-	
+
 	public Client(int portServer)
 	{
-		
 	}
-	
+
+	public String getMachineName()
+	{
+
+		return null;
+	}
+
+	public int getPortNumber()
+	{
+
+		return 0;
+	}
+
+	public void connectToServer(String machineName, int portNum) throws UnknownHostException, IOException
+	{
+		clientSocket = new Socket(machineName, portNum);
+	}
+
 	//function to setup the streams
 	public void setupStreams(Socket clientSocket) throws IOException
 	{
-		 output = new PrintWriter(clientSocket.getOutputStream(), true); 
-		 input = new BufferedReader(new InputStreamReader( clientSocket.getInputStream())); 
+		output = new PrintWriter(clientSocket.getOutputStream(), true); 
+		input = new BufferedReader(new InputStreamReader( clientSocket.getInputStream())); 
 	}
-	
+
 	public void receiveData(String retreivedData) throws IOException
 	{
 		String label = null;
-		
+
 		while((label = input.readLine()) != null)
 		{
 			retreivedData = label;
@@ -33,9 +49,17 @@ public class Client {
 		}
 	}
 	
+
 	public void sendData(String data)
 	{
 		output.println(data);
 		System.out.println("Client sent: " + data + "\n");
+	}
+
+	public void closeConnections() throws IOException
+	{
+		output.close();
+		input.close();
+		clientSocket.close();
 	}
 }
